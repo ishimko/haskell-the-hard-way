3339588445721d4bfa53aebe4f3e94b017e97852
vanashimko <vana.shimko@gmail.com>
2017-08-18 18:42:02 +0300

    Add data type example

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,11 @@
 module Main where
     main :: IO ()
-    main = print (evenSum [1, 2, 3])
+    main = do
+        print $ evenSum [1, 2, 3]
+        print User { lastName = "Shimko", firstName = "Ivan"}
 
     evenSum :: Integral a => [a] -> a
     evenSum = sum . filter even
+
+    data User = User {firstName :: String, lastName :: String }
+                deriving Show
