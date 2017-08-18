1f90cd8b9f61eabd3dfc381acf8f743eff94dfc9
vanashimko <vana.shimko@gmail.com>
2017-08-18 19:59:37 +0300

    Add binary tree

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
     main :: IO ()
     main = do
         print $ evenSum [1, 2, 3]
         print User { lastName = "Shimko", firstName = "Ivan"}
+        print $ treeFromList [4, 2, 1, 3, 6, 5, 7]
 
     evenSum :: Integral a => [a] -> a
     evenSum = sum . filter even
 
     data User = User {firstName :: String, lastName :: String }
-                deriving Show
+                    deriving Show
+
+    data BinaryTree a = Empty
+                        | Node a (BinaryTree a) (BinaryTree a)
+                            deriving Show
+    treeFromList :: forall a. (Ord a) => [a] -> BinaryTree a
+    treeFromList [] = Empty
+    treeFromList (x:xs) = Node x (subtree (<))
+                                 (subtree (>))
+                            where subtree comparer = treeFromList $ filter (comparer x) xs
+                                  subtree :: (a -> a -> Bool) -> BinaryTree a
+
+                                  
